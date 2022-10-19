88cda53bf78606f78ada28c464de84eae57f34f1
emmanuelthegeek <altshiftctrl6@gmail.com>
2022-10-19 11:30:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple-Todo-Application-built-in-Laravel
+This is a basic todo application in Laravel.
+Backend -> Laravel, Livewire
+Frontend -> HTML, CSS, Alpine.js
+Database -> SQlite
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+};
